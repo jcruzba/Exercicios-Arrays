8a6f1019920904ee4f420ea9c00ecfc150732272
jcruzba <114691251+jcruzba@users.noreply.github.com>
2022-10-29 01:08:11 -0300

    Initial commit

---
+++ src/Ex4_ArrayMultidimensional.java
@@ -0,0 +1,27 @@
+/*
+Gere e imprima uma matriz M 4X4 com valores aleatórios entre 0-9.
+ */
+
+import java.util.Random;
+
+public class Ex4_ArrayMultidimensional {
+    public static void main(String[] args){
+        Random rand = new Random();
+
+        int[] [] M = new int[4][4];
+
+        for (int i = 0; i < M.length; i++){
+            for (int j = 0; j < M[i].length; j++){
+                M[i][j] = rand.nextInt(9);
+
+            }
+        }
+        System.out.println("Matriz: ");
+        for (int[] linha : M ) {
+            for (int coluna : linha) {
+                System.out.print(coluna + " ");
+            }
+            System.out.println();
+        }
+    }
+}
